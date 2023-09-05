5ecfe8f937f8e5c8bf36a738df016514f53233db
Allon Nam <allon.nam96@gmail.com>
2023-09-05 17:03:14 -0400

    progress on board

--- board.rb
+++ board.rb
@@ -3,4 +3,40 @@ class Board
         @size = size
         @grid = Array.new(size){Array.new(size,"-")}
     end 
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos,value)
+        row, col = pos
+        @grid[row][col] = value
+    end
+
+    def empty?(pos)
+        self[pos] == '_'
+    end
+
+    def populate
+        num_cards = (@size * @size) / 2
+        card = []
+        num_cards.times do
+       card_value = ('a'..'z').to_a.sample(5).join('')
+       card << Card.new(card_value)
+        end
+    rand_card = card.sample
+    if @grid.flatten.count(rand_card) < 2
+        i = 0
+        while i < @size
+            j = 0
+            while j < @size
+                @grid[i][j] = 
+                j += 1
+            end
+            i += 1
+        end
+    end
+    end
+
 end 
